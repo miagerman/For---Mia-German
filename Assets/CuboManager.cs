6048a6a9bafe7256bb4b40e8be106908af27a93a
miagerman <49194476@est.ort.edu.ar>
2025-06-17 09:05:34 -0300

    clase 17/6

---
+++ Assets/CuboManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuboManager : MonoBehaviour
+{
+    public CuboScript [] cuboScripts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < cuboScripts.Length; i++)
+        {
+            Debug.Log(cuboScripts[0].esPersistente);
+            if (cuboScripts[0].esPersistente !=true)
+            {
+                Destroy(cuboScripts[i].gameObject);
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
